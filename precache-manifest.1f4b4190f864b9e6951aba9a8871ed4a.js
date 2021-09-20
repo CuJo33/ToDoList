772c9743c32b9df584b6be39e5c16d7fec957f5e
CuJo33 <77585826+CuJo33@users.noreply.github.com>
2021-09-20 12:09:11 +0100

    Updates

---
+++ precache-manifest.1f4b4190f864b9e6951aba9a8871ed4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fb3195a9a2461073566c53d6417085",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "ec7f11cbb5b84faddce4",
+    "url": "/Todolist/static/css/2.46b578d6.chunk.css"
+  },
+  {
+    "revision": "b881c6b4e1c44d58d13d",
+    "url": "/Todolist/static/css/main.789df585.chunk.css"
+  },
+  {
+    "revision": "ec7f11cbb5b84faddce4",
+    "url": "/Todolist/static/js/2.47e4a343.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Todolist/static/js/2.47e4a343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b881c6b4e1c44d58d13d",
+    "url": "/Todolist/static/js/main.d9c49fb6.chunk.js"
+  },
+  {
+    "revision": "c22257e6cef199439620",
+    "url": "/Todolist/static/js/runtime-main.69ffe81f.js"
+  },
+  {
+    "revision": "7f2606f07c1086c3b3496abfb4b0c27b",
+    "url": "/Todolist/static/media/cujo_head.7f2606f0.jpg"
+  }
+]);
